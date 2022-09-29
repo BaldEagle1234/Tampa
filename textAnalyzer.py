b05e4b19d14f899b01ce7a1cf65cdea070ac1cc9
BaldEagle1234 <114659021+BaldEagle1234@users.noreply.github.com>
2022-09-29 20:16:31 +0900

    Add files via upload

--- textAnalyzer.py
+++ textAnalyzer.py
@@ -7,6 +7,18 @@ def textAnalyzer():
 
 	print("This is my text analyzer")
 
+
+	# file open
+
+
+	with open("example.txt", "r") as f:
+
+		line = f.readlines()
+		for l in line:
+			print(l.strip())
+
+
+
 	print("the top 5 most frequently used words in the file ")
 	print("the number of times the top 5 words are used")
 	print("should be sorted by most frequently used count")
