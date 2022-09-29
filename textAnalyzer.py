c0656a8ceca522ca13758c9efcad956a6e8e46bb
BaldEagle1234 <114659021+BaldEagle1234@users.noreply.github.com>
2022-09-29 20:05:17 +0900

    Add files via upload

--- textAnalyzer.py
+++ textAnalyzer.py
@@ -4,5 +4,10 @@ def textAnalyzer():
 
 	print("This is my text analyzer")
 
+	print("the top 5 most frequently used words in the file ")
+	print("the number of times the top 5 words are used")
+	print("should be sorted by most frequently used count")
+	print("the longest word in the document")
+	print("the average word size")
 
 textAnalyzer()
